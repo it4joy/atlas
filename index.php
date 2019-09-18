7dd303cfe025d657d1b93662ce94e3d3acd76251
Igor Ivanov <drkierkegor@gmail.com>
2019-09-18 17:30:18 +0300

    1st commit

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <title>Atlas - test task</title>
+
+        <link href="css/main.css" rel="stylesheet" type="text/css">
+        <script src="js/main.js" type="text/javascript" defer></script>
+    </head>
+
+    <body>
+        <div class="app-wrapper">
+            <noscript>
+                <p>Пожалуйста, включите поддержку JavaScript в Вашем браузере!</p>
+            </noscript>
+            <div class="container">
+                
+            </div>
+
+            <a href="#" class="scrollup">
+                
+            </a>
+        </div>
+    </body>
+</html>
