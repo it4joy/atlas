a7b33868feefc234156d4baed7c8028b2c751e6e
Igor Ivanov <drkierkegor@gmail.com>
2019-09-20 20:08:12 +0300

    updt logic: validation, adding record

--- public/js/main.js
+++ public/js/main.js
@@ -1,6 +1,6 @@
 'use strict';
 
-$( document ).ready(function() {
+$(document).ready(function() {
     const errors = {
         emptyFields: 'Заполните, пожалуйста, поле.',
         invalidEmail: 'Email необходимо ввести в формате (пример): example@domain.com (без пробелов).',
@@ -28,10 +28,27 @@ $( document ).ready(function() {
         emailRegExp: /^([a-zA-Z0-9_\.\-\+])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/i,
     };
 
+    // gets records for page 'records.html'
+    const getRecords = () => {
+        $.ajax({
+            method: 'POST',
+            url: 'php/app.php',
+            data: {
+                action: 'get_records'
+            },
+            
+        });
+    };
+
+    // gets the name of browser
+    const browser = navigator.userAgent;
+
+    // form elements
     const userInfoForm = $('#user-info-form');
     const btnSave = userInfoForm.find('.btn-save');
     const userFullName = userInfoForm.find('#fullname');
     const userEmail = userInfoForm.find('#email');
+    const userBirthday = userInfoForm.find('#birthday');
     const docType = userInfoForm.find('#doc-type');
     const docNumber = userInfoForm.find('#doc-number');
 
@@ -45,10 +62,12 @@ $( document ).ready(function() {
         // ckecks required fields
         const userFullNameVal = userFullName.val();
         const userEmailVal = userEmail.val();
+        const userBirthdayVal = userBirthday.val();
         const docNumberVal = docNumber.val();
 
         const formDataAsArray = userInfoForm.serializeArray();
-        console.log(formDataAsArray); // test
+        //console.log(formDataAsArray); // test
+        //console.log(userBirthdayVal); // test
 
         if (userFullNameVal.length === 0) {
             outputError(userInfoForm, userFullName, 'emptyFields');
@@ -56,15 +75,16 @@ $( document ).ready(function() {
             outputError(userInfoForm, userEmail, 'emptyFields');
         } else if ( userEmailVal.search(regExps.emailRegExp) === -1 ) {
             outputError(userInfoForm, userEmail, 'invalidEmail');
+        } else if (userBirthdayVal.length === 0) {
+            outputError(userInfoForm, userBirthday, 'emptyFields');
         } else {
             requiredFields = true;
         }
 
         if (requiredFields === true) {
-            // do request (user can send form with only required fields)
-            alert('You filled required fields. That\'s OK.'); // test
+            //alert('You filled required fields. That\'s OK.'); // test
             
-            // gets values from additional fields if they have values
+            // ckecks additional fields if they have values
             let counter = 0;
 
             $.each(formDataAsArray, function(index, item) {                
@@ -75,13 +95,35 @@ $( document ).ready(function() {
 
             console.log(counter); // test
 
-            if (counter > 3) {
-                alert('The amount of items in array of form data is more than 3.'); // test
-
-                if ( docNumberVal.match(regExps.numberRegExp) === null ) {
+            if (counter > 4) {
+                //alert('The amount of items in array of form data is more than 4.'); // test
+                if (docNumberVal.length === 0) {
+                    outputError(userInfoForm, docNumber, 'emptyFields');
+                    return false;
+                } else if ( docNumberVal.match(regExps.numberRegExp) === null ) {
                     outputError(userInfoForm, docNumber, 'notANumber');
+                    return false;
                 }
             }
+
+            // makes a request in any case
+            const postData = userInfoForm.serialize() + `&action=add_record&browser=${browser}`;
+
+            $.ajax({
+                method: 'POST',
+                url: 'php/app.php',
+                data: postData,
+                success: function(data) {
+                    const response = $.parseJSON(data);
+                    alert(response);
+                },
+                error: function(jqxhr, status, errMsg) {
+                    console.log(`Статус: ${status}. Ошибка: ${errMsg}`);
+                    alert(errMsg); // test
+                }
+            });
+
+            userInfoForm.trigger('reset');
         }
     });
 });
--- public/php/app.php
+++ public/php/app.php
@@ -2,22 +2,82 @@
 
 require_once("db.php");
 
+$errArray = array(
+    "emptyFields" => "Заполните, пожалуйста, поле: ",
+    "invalidEmail" => "Email необходимо ввести в формате (пример): example@domain.com (без пробелов).",
+    "notANumber" => "Пожалуйста, введите числа.",
+    "requestFailure" => "Произошла ошибка, попробуйте отправить запись позже.",
+);
+
+// regExps
+$numberRegExp = "/^\d\d+\d$/";
+$emailRegExp = "/^([a-zA-Z0-9_\.\-\+])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/i";
+
+$userIP = $_SERVER['REMOTE_ADDR'];
 
 if ( !empty($_POST) ) {
     // adds a record
-    if ( $_POST["action"] === "add" ) {
-        $name = $_POST["name"];
+    if ( $_POST["action"] === "add_record" ) {
+        $fullname = $_POST["fullname"];
         $email = $_POST["email"];
-        $message = $_POST["message"];
+        $birthday = $_POST["birthday"];
+        $gender = $_POST["gender"];
+        $docType = $_POST["doc-type"];
+        $docNumber = $_POST["doc-number"];
+        $rights = $_POST['rights'];
+        $browser = $_POST['browser'];
 
-        $name = mysqli_real_escape_string($link, $name);
+        if ( empty($fullname) ) {
+            echo json_encode( $errArray["emptyFields"] . "ФИО.");
+        } else if ( empty($email) ) {
+            echo json_encode( $errArray["emptyFields"] . "Email.");
+        } else if ( preg_match($emailRegExp, $email) !== 1 ) {
+            echo json_encode( $errArray["invalidEmail"] );
+        } else if ( empty($birthday) ) {
+            echo json_encode( $errArray["emptyFields"] . "Дата рождения.");
+        }
+        
+        if ( !empty($docNumber) ) {
+            if ( preg_match($numberRegExp, $docNumber) !== 1 ) {
+                echo json_encode( $errArray["notANumber"] );
+            }
+        }
+
+        // trims spaces
+        $fullname = trim($fullname);
+        $docType = trim($docType);
+
+        // converts to string
+        $rightsConverted = implode(", ", $rights);
+        print_r($rightsConverted); // test
+
+        // provides safety
+        $fullname = mysqli_real_escape_string($link, $fullname);
         $email = mysqli_real_escape_string($link, $email);
-        $message = mysqli_real_escape_string($link, $message);
+        $birthday = mysqli_real_escape_string($link, $birthday);
+        $gender = mysqli_real_escape_string($link, $gender);
+        $docType = mysqli_real_escape_string($link, $docType);
+        $docNumber = mysqli_real_escape_string($link, $docNumber);
+        $rightsConverted = mysqli_real_escape_string($link, $rightsConverted);
 
-        $query = "INSERT INTO Guest_book(Name, Email, Message) VALUES('" . $name . "', '" . $email . "', '" . $message . "')";
+        $query = "INSERT INTO Users(Full_Name, Email, Birthday, Gender, Doc_Type, Doc_Number, Rights, User_IP, Browser) VALUES('$fullname', '$email', '$birthday', '$gender', '$docType', '$docNumber', '$rightsConverted', '$userIP', '$browser')";
 
         if ( mysqli_query($link, $query) ) {
-            echo json_encode( array("response_success" => $name . ", Ваше сообщение отправлено!") );
+            echo json_encode("Запись добавлена!");
+        } else {
+            echo json_encode( $errArray["requestFailure"] );
+        }
+    } else if ( $_POST["action"] === "get_records" ) {
+        $query = "SELECT * FROM Users ORDER BY Time_Stamp DESC";
+
+        if ( $result = mysqli_query($link, $query) ) {
+            $records = array();
+
+            while ( $row = mysqli_fetch_assoc($result) ) {
+                $records[] = $row;
+            }
+
+            echo json_encode($records);
         }
     }
 }
